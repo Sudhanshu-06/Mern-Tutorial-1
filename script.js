3c2dd77dfdb1bca568777d7acc697ab48a589638
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-06-26 16:42:40 +0530

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+function updateCard(){
+     let schoolNameTxt=document.getElementById("schoolName").value;
+     
+     let studentNameTxt=document.getElementById("studentName").value;
+     
+     let doBTxt=document.getElementById("dob").value;
+     
+     let classTxt=document.getElementById("class").value;
+     
+     let rollnoTxt=document.getElementById("rollno").value;
+     
+     let contactNoTxt=document.getElementById("contactno").value;
+     
+     console.log(schoolNameTxt,studentNameTxt,doBTxt,classTxt,rollnoTxt,contactNoTxt);
+     
+     
+    
+    
+}
+// function clearAll(){
+//     document.getElementById("schoolName").value="";
+     
+//     document.getElementById("studentName").value="";
+    
+//     document.getElementById("dob").value="";
+    
+//     document.getElementById("class").value="";
+    
+//     document.getElementById("rollno").value="";
+    
+//     document.getElementById("contactno").value="";
+
+// }
