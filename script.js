d6768e90431b887d79e9817732309fd65cea9988
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-07-07 00:52:24 +0530

    done

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function updateCard(){
-     document.getElementById("schoolNameVal").value=document.getElementById("schoolName").value;
+     document.getElementById("schoolNameVal").innerText=document.getElementById("schoolName").value;
      
-     let studentNameTxt=document.getElementById("studentName").value;
+     document.getElementById("studnetNameVal").innerText=document.getElementById("studentName").value;
      
-     let doBTxt=document.getElementById("dob").value;
+     document.getElementById("dobVal").innerText=document.getElementById("dob").value;
      
-     let classTxt=document.getElementById("class").value;
+     document.getElementById("classVal").innerText=document.getElementById("class").value;
      
-     let rollnoTxt=document.getElementById("rollno").value;
+     document.getElementById("rollnoVal").innerText=document.getElementById("rollno").value;
      
-     let contactNoTxt=document.getElementById("contactno").value;
+     document.getElementById("contactnoVal").innerText=document.getElementById("contactno").value;
      
      // console.log(schoolNameTxt,studentNameTxt,doBTxt,classTxt,rollnoTxt,contactNoTxt);
      
+}
+function printIdCard(){
+     window.print();
 }
 
 // function clearAll(){
